8abac9656733fcf161ddcee1f52da9e502a2f00e
Koptev, Roman <forest_software@mail.ru>
2020-06-27 05:17:11 +0300

    for new pipelines

--- dicountries/__init__.py
+++ dicountries/__init__.py
@@ -1,5 +1,11 @@
 from .metadata import version as __version__
-from .dict_index import *
-from .loader import *
-from .utils import *
-from .whoosh_index import *
+
+def _need_import():
+    import __main__
+    return __main__.__file__ != 'setup.py'
+
+if _need_import():
+    from .dict_index import *
+    from .loader import *
+    from .utils import *
+    from .whoosh_index import *
--- dicountries/metadata.py
+++ dicountries/metadata.py
@@ -4,12 +4,12 @@ version_file = os.path.join(os.path.dirname(__file__), 'version.py')
 if os.path.isfile(version_file):
     from .version import __version__ as version
 else:
-    version = '0.0.1dev2'
+    version = 'dev'
 
 name = 'dicountries'
 copyright = '2020, SoftwareONE'
 author = 'Koptev, Roman'
 description = 'Common library to normalize country names for DI project'
 license = 'SWO'
-url = 'https://dev.azure.com/swodataintelligence/Data%20Intelligence%20Scrum/_git/runtime-common-library-dicountries'
+url = 'https://swodataintelligence@dev.azure.com/swodataintelligence/Data%20Intelligence%20Scrum/_git/runtime-common-library-dicountries'
 author_email = 'roman.koptev@softwareone.com'
